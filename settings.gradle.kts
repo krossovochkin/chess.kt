5d0fd6c685bde24c9a2c29a6d51c4e1027c0a849
Vasya Drobushkov <vasya.drobushkov@gmail.com>
2024-11-27 06:26:29 +0100

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,8 +8,7 @@ pluginManagement {
 
 plugins {
     // See https://jmfayard.github.io/refreshVersions
-    id("de.fayard.refreshVersions") version "0.60.4"
-////                            # available:"0.60.5"
+    id("de.fayard.refreshVersions") version "0.60.5"
 }
 
 rootProject.name = "chesskt"
