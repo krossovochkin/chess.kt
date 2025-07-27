b67d322341a45aebafb2739d6fd133ad66684199
Vasya Drobushkov <vasya@drobushkov.com>
2025-07-27 10:16:03 +0200

    Improve demo in wasm by handling client screen size

--- composeApp/src/wasmJsMain/kotlin/com/krossovochkin/chesskt/main.kt
+++ composeApp/src/wasmJsMain/kotlin/com/krossovochkin/chesskt/main.kt
@@ -3,11 +3,13 @@ package com.krossovochkin.chesskt
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
 import kotlinx.browser.document
+import kotlin.math.min
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 
 @OptIn(ExperimentalComposeUiApi::class, ExperimentalResourceApi::class)
 fun main() {
-    ComposeViewport(document.body!!) {
-        App()
+    val body = document.body!!
+    ComposeViewport(body) {
+        App(screenSizePx = min(body.clientWidth, body.clientHeight))
     }
 }
