f18a0c1fbd8c5aceee4a9905f5b05a481e18f9d4
Vasya Drobushkov <vasya@drobushkov.com>
2025-07-27 10:29:37 +0200

    Improve wasm demo: use window sizes

--- composeApp/src/wasmJsMain/kotlin/com/krossovochkin/chesskt/main.kt
+++ composeApp/src/wasmJsMain/kotlin/com/krossovochkin/chesskt/main.kt
@@ -4,12 +4,12 @@ import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
 import kotlinx.browser.document
 import kotlin.math.min
+import kotlinx.browser.window
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 
 @OptIn(ExperimentalComposeUiApi::class, ExperimentalResourceApi::class)
 fun main() {
-    val body = document.body!!
-    ComposeViewport(body) {
-        App(screenSizePx = min(body.clientWidth, body.clientHeight))
+    ComposeViewport(document.body!!) {
+        App(screenSizePx = min(window.innerWidth, window.innerHeight))
     }
 }
