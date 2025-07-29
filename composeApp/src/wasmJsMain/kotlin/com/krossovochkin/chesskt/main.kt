8e304057e943c8b85b86d6d6c2e252c760c3f00f
Vasya Drobushkov <vasya.drobushkov@gmail.com>
2025-07-29 16:19:05 +0200

    Update main.kt

--- composeApp/src/wasmJsMain/kotlin/com/krossovochkin/chesskt/main.kt
+++ composeApp/src/wasmJsMain/kotlin/com/krossovochkin/chesskt/main.kt
@@ -1,15 +1,37 @@
 package com.krossovochkin.chesskt
 
+import androidx.compose.runtime.*
+import androidx.compose.material.Text
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
 import kotlinx.browser.document
-import kotlin.math.min
 import kotlinx.browser.window
+import kotlin.math.min
 import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.w3c.dom.HTMLElement
 
 @OptIn(ExperimentalComposeUiApi::class, ExperimentalResourceApi::class)
 fun main() {
     ComposeViewport(document.body!!) {
-        App(screenSizePx = min(window.innerWidth, window.innerHeight))
+        var size by remember { mutableStateOf<Int?>(null) }
+
+        LaunchedEffect(Unit) {
+            size = getContainerSize()
+        }
+
+        if (size != null) {
+            App(screenSizePx = size!!)
+        } else {
+            Text("Loading...")
+        }
+    }
+}
+
+private fun getContainerSize(): Int {
+    val iframe = window.asDynamic().frameElement as? HTMLElement
+    return if (iframe != null) {
+        min(iframe.offsetWidth, iframe.offsetHeight)
+    } else {
+        min(window.innerWidth, window.innerHeight)
     }
 }
