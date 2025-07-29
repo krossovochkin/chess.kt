f58469bf6a3c27dabe03b240fe631ca9d098f827
Vasya Drobushkov <vasya.drobushkov@gmail.com>
2025-07-29 16:32:44 +0200

    Update main.kt

--- composeApp/src/wasmJsMain/kotlin/com/krossovochkin/chesskt/main.kt
+++ composeApp/src/wasmJsMain/kotlin/com/krossovochkin/chesskt/main.kt
@@ -13,25 +13,28 @@ import org.w3c.dom.HTMLElement
 @OptIn(ExperimentalComposeUiApi::class, ExperimentalResourceApi::class)
 fun main() {
     ComposeViewport(document.body!!) {
-        var size by remember { mutableStateOf<Int?>(null) }
-
-        LaunchedEffect(Unit) {
-            size = getContainerSize()
-        }
+        val size = remember { getContainerSize() }
 
         if (size != null) {
-            App(screenSizePx = size!!)
+            App(screenSizePx = size)
         } else {
             Text("Loading...")
         }
     }
 }
 
-private fun getContainerSize(): Int {
-    val iframe = window.asDynamic().frameElement as? HTMLElement
+private fun getContainerSize(): Int? {
+    val iframe = windowWithFrame.frameElement as? HTMLElement
     return if (iframe != null) {
         min(iframe.offsetWidth, iframe.offsetHeight)
     } else {
         min(window.innerWidth, window.innerHeight)
-    }
+    }.takeIf { it > 0 }
+}
+
+@JsName("window")
+external val windowWithFrame: WindowWithFrameElement
+
+external interface WindowWithFrameElement : org.w3c.dom.Window {
+    val frameElement: org.w3c.dom.Element?
 }
